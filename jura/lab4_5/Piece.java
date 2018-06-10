15c24334cb4f83e689203786dcff70cf1c140c8e
Igor Boyarshin <igor.boyarshin@gmail.com>
2018-06-10 15:05:44 +0300

    updated

--- jura/lab4_5/Piece.java
+++
@@ -1,9 +0,0 @@
-public class Piece {
-    public final PieceType pieceType;
-    public final Type type;
-
-    public Piece(PieceType pieceType, Type type) {
-        this.pieceType = pieceType;
-        this.type = type;
-    }
-}
--- jura/lab6/run.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/zsh
-
-javac Main.java
-
-java Main
-
-rm -f *.class
